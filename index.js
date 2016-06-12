d248a8cc4fbc8d3aa0e92315180e25ced1f93bd4
Howard Yeh <howard@metacircus.com>
2016-06-12 09:07:50 -0700

    add discourse thread info

--- index.js
+++ index.js
@@ -2,41 +2,49 @@ module.exports = {
   lessons: {
     "intro": {
       repo: "rn-timer",
+      discourse_id: 1881,
     },
 
     "timer": {
       repo: "rn-timer",
+      discourse_id: 1882,
     },
 
     "trace-update-cycle": {
       repo: "rn-timer",
+      discourse_id: 1883,
     },
 
 
     "scrollview": {
       repo: "rn-tumblr",
+      discourse_id: 1884,
     },
 
     "tumble-photopost-layout": {
       repo: "rn-tumblr",
+      discourse_id: 1885,
     },
 
     "cross-platform-pager": {
       repo: "rn-tumblr",
+      discourse_id: 1886,
     },
 
     "tumble-image-modal": {
       repo: "rn-tumblr",
+      discourse_id: 1887,
     },
 
     "animated-intro": {
       repo: "rn-tumblr",
+      discourse_id: 1888,
     },
 
     "tumble-animate-image-modal": {
       repo: "rn-tumblr",
+      discourse_id: 1889,
     },
   }
 }
-
 
